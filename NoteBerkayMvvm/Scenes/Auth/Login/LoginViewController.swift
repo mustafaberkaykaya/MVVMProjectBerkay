db3368334a5c5958474d897fee285b88185c0aba
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-15 18:40:18 +0300

    add customTextField

--- NoteBerkayMvvm/Scenes/Auth/Login/LoginViewController.swift
+++ NoteBerkayMvvm/Scenes/Auth/Login/LoginViewController.swift
@@ -13,13 +13,21 @@ final class LoginViewController: BaseViewController<LoginViewModel> {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       // let headerView = HeaderView()
-        //headerView.descriptionText = "Login or sign up to continue using our app."
-       // headerView.titleText = "Login"
-       // view.addSubview(headerView)
-       // headerView.leadingToSuperview().constant = 42
-       // headerView.trailingToSuperview().constant = -41
-       // headerView.topToSuperview().constant = 103
+//        let headerView = HeaderView()
+//        headerView.descriptionText = "Login or sign up to continue using our app."
+//        headerView.titleText = "Login"
+//        view.addSubview(headerView)
+//        headerView.leadingToSuperview().constant = 42
+//        headerView.trailingToSuperview().constant = -41
+//        headerView.topToSuperview().constant = 103
+        
+//        let textField = AuthTextField()
+//        textField.placeholder = "Deneme"
+//        view.addSubview(textField)
+//        textField.centerYToSuperview()
+//        textField.leadingToSuperview().constant = 25
+//        textField.trailingToSuperview().constant = -25
+
+       
     }
-    
 }
