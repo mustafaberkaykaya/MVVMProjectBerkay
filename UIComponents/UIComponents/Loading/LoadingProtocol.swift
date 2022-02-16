129e773007ce61e0c53455b530f4c459edb7694f
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-16 15:50:22 +0300

    add loading protocol

---
+++ UIComponents/UIComponents/Loading/LoadingProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingProtocol.swift
+//  UIComponents
+//
+//  Created by Mustafa Berkay Kaya on 16.02.2022.
+//
+
+import UIKit
+
+public protocol LoadingProtocol {
+    func presentLoading()
+    func dismissLoading()
+}
+
+public extension LoadingProtocol where Self: UIViewController {
+    func presentLoading() {
+           let window = UIApplication.shared.windows.first
+           window?.startBlockingActivityIndicator()
+       }
+       
+       func dismissLoading() {
+           let window = UIApplication.shared.windows.first
+           window?.stopBlockingActivityIndicator()
+       }
+}
