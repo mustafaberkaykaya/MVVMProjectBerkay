b834e0efb65b9e551f10a87367458a98f2db70dc
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-16 14:33:15 +0300

    add new closures

---
+++ Utilities/Utilities/Constants/Closures/Closures.swift
@@ -0,0 +1,11 @@
+//
+//  Closures.swift
+//  Utilities
+//
+//  Created by Mustafa Berkay Kaya on 16.02.2022.
+//
+
+import Foundation
+
+public typealias VoidClosure = (() -> Void)
+public typealias StringClosure = ((String) -> Void)
