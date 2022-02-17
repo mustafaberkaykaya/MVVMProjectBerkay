07d457ab16c82171de11b57be98d19a1aa2065ed
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-17 09:50:07 +0300

    add mainNavigationController

--- NoteBerkayMvvm/Scenes/Base/MainNavigationController.swift
+++ NoteBerkayMvvm/Scenes/Base/MainNavigationController.swift
@@ -16,6 +16,47 @@ class MainNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureContents()
     }
 
+    private func configureContents() {
+        let backImage = UIImage.icBackArrow
+            .resize(to: .init(width: 20, height: 16))
+            .withRenderingMode(.alwaysTemplate)
+            .withAlignmentRectInsets(.init(top: 0, left: 0, bottom: 0, right: 0))
+        let titleTextAttributes = AttributedStringDictionaryBuilder()
+            .font(.font(.josefinSansBold, size: 13))
+            .foregroundColor(.appEbonyClay)
+            .build()
+        navigationBar.barTintColor = .appWhite
+        navigationBar.shadowImage = UIImage()
+        navigationBar.tintColor = .appEbonyClay
+        navigationBar.titleTextAttributes = titleTextAttributes
+        UIBarButtonItem.appearance()              .setTitleTextAttributes(AttributedStringDictionaryBuilder()
+            .font(.font(.josefinSansSemiBold, size: 14))
+            .foregroundColor(.appWhite)
+            .build(), for: .normal)
+        navigationBar.backIndicatorImage = backImage
+        navigationBar.backIndicatorTransitionMaskImage = backImage
+
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.shadowColor = .clear
+            appearance.backgroundColor = .appWhite
+            appearance.titleTextAttributes = titleTextAttributes
+            appearance.setBackIndicatorImage(backImage, transitionMaskImage: backImage)
+            navigationBar.standardAppearance = appearance
+            navigationBar.scrollEdgeAppearance = appearance
+            navigationBar.compactAppearance = appearance
+        }
+        
+        navigationBar.backItem?.backBarButtonItem?.setTitlePositionAdjustment(.init(horizontal: 0, vertical: 0), for: .default)
+    }
+
+    #if DEBUG
+    deinit {
+        debugPrint("deinit \(self)")
+    }
+    #endif
 }
