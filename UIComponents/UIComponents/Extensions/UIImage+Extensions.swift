a7206f4f053df4495c8c591db1a5e5aec740f7d6
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-17 09:46:26 +0300

    add extension

--- UIComponents/UIComponents/Extensions/UIImage+Extensions.swift
+++ UIComponents/UIComponents/Extensions/UIImage+Extensions.swift
@@ -7,4 +7,15 @@
 
 import Kingfisher
 
-
+public extension UIImage {
+    func resize(to size: CGSize, for contentMode: UIView.ContentMode? = nil) -> UIImage {
+        switch contentMode {
+        case .scaleAspectFit:
+            return kf.resize(to: size, for: .aspectFit)
+        case .scaleToFill:
+            return kf.resize(to: size, for: .aspectFill)
+        default:
+            return kf.resize(to: size)
+        }
+    }
+}
