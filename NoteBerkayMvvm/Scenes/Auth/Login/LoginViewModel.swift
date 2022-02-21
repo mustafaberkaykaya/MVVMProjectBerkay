e463627c5fbee72f8ea16201af653360d3bef0fa
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-21 17:13:12 +0300

    add showLoading

--- NoteBerkayMvvm/Scenes/Auth/Login/LoginViewModel.swift
+++ NoteBerkayMvvm/Scenes/Auth/Login/LoginViewModel.swift
@@ -24,14 +24,16 @@ final class LoginViewModel: BaseViewModel<LoginRouter>, LoginViewProtocol {
     let keychain = KeychainSwift()
    
     func sendLoginRequest(email: String, password: String) {
+        showLoading?()
         dataProvider.request(for: LoginRequest(email: email, password: password)) { [weak self] result in
             guard let self = self else { return }
+            self.hideLoading?()
             switch result {
             case.success(let response):
                 self.keychain.set(response.data?.accessToken ?? "", forKey: Keychain.token)
                 print("başarılı login")
-            case .failure(let error):
-                self.showWarningToast?("\(error.localizedDescription) \("Hata")")
+            case .failure:
+                self.showWarningToast?(L10n.Login.error1)
             }
         }
     }
