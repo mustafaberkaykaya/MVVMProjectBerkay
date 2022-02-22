b2f86b6fe91a2127d7cb692e72ce0dc3d5307a97
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-22 12:35:47 +0300

    minor fix

--- DataProvider/DataProvider/Requests/Model/ForgotPassword.swift
+++ DataProvider/DataProvider/Requests/Model/ForgotPassword.swift
@@ -1,14 +1,12 @@
 //
-//  ResetPassword.swift
+//  ForgotPassword.swift
 //  DataProvider
 //
 //  Created by Mustafa Berkay Kaya on 22.02.2022.
 //
 
-
-public struct ResetPassword: Codable {
+public struct ForgotPassword: Codable {
     public let code: String?
     public let data: DataClass?
     public let message: String?
 }
-
