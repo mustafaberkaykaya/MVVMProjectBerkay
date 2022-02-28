0ec5e80636d5d0f6e463b1985434defb44b359b5
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-28 12:49:41 +0300

    add textfield extension

---
+++ UIComponents/UIComponents/Extensions/UITextField+Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  UITextField+Extensions.swift
+//  UIComponents
+//
+//  Created by Mustafa Berkay Kaya on 28.02.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+  
+  public enum Direction {
+    case left
+    case right
+  }
+  // add image to textfield
+public func withImage(direction: Direction, image: UIImage, colorSeparator: UIColor, colorBorder: UIColor, backgroundColor: UIColor) {
+    let mainView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 40))
+    mainView.layer.cornerRadius = 5
+    let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 40))
+    view.backgroundColor = .white
+    view.clipsToBounds = true
+    view.layer.cornerRadius = 5
+    view.layer.borderWidth = CGFloat(0.5)
+    view.layer.borderColor = UIColor.white.cgColor
+    view.backgroundColor = backgroundColor
+    mainView.addSubview(view)
+    let imageView = UIImageView(image: image)
+    imageView.contentMode = .scaleAspectFit
+    imageView.frame = CGRect(x: 15.0, y: 14.0, width: 12.0, height: 12.0)
+    view.addSubview(imageView)
+    let seperatorView = UIView()
+    seperatorView.backgroundColor = colorSeparator
+    mainView.addSubview(seperatorView)
+
+    if (Direction.left == direction) { // image left
+      seperatorView.frame = CGRect(x: 45, y: 0, width: 1, height: 40)
+      self.leftViewMode = .always
+      self.leftView = mainView
+    } else { // image right
+      seperatorView.frame = CGRect(x: 0, y: 0, width: 1, height: 45)
+      self.rightViewMode = .always
+      self.rightView = mainView
+    }
+    self.layer.borderColor = colorBorder.cgColor
+    self.layer.borderWidth = CGFloat(0.5)
+    self.layer.cornerRadius = 5
+  }
+}
+
