58b4dc2430ce2f7253a88fbc8efe44bf1cc864a7
Berkay  Kaya <mberkaykaya@outlook.com>
2022-03-01 15:06:57 +0300

    add getUser request

---
+++ DataProvider/DataProvider/Requests/Auth/GetUserRequest.swift
@@ -0,0 +1,19 @@
+//
+//  GetUserRequest.swift
+//  DataProvider
+//
+//  Created by Mustafa Berkay Kaya on 1.03.2022.
+//
+
+public struct GetUserRequest: RequestProtocol {
+    
+    public typealias ResponseType = <#T##Type###>
+    
+    public var path: String = <#T##Type###>
+    public var method: RequestMethod = <#T##Type###>
+    public var parameters: RequestParameters = [:]
+    public var headers: RequestHeaders = [:]
+    
+    public init() {}
+    
+}
---
+++ DataProvider/DataProvider/Requests/Model/GetUserResponse.swift
@@ -0,0 +1,8 @@
+//
+//  GetUserResponse.swift
+//  DataProvider
+//
+//  Created by Mustafa Berkay Kaya on 1.03.2022.
+//
+
+import Foundation
